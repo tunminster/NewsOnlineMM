795f192221e3b3735b6a03b9f39bdd347a8fdaf8
tunminster <tunminster@gmail.com>
2016-03-30 16:56:00 +0100

    Initial commit

---
+++ NewsOnlineMM.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsOnlineMM.Web.Startup))]
+namespace NewsOnlineMM.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
